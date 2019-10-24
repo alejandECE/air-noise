f44344e799ad52e8d1585c19e7f9c80d42b2490f
alejandECE <alejand@umich.edu>
2019-10-24 09:57:37 -0400

    Added csv exploration related scripts

--- python/aircraft_reports.py
+++ python/aircraft_reports.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def display_random_subset(X,y,count=9,order='C',shape=(20,34),labels=[]):
+def display_random_subset(X,y,count=9,order='C',mfcc=20):
     indexes = np.random.permutation(X.shape[0])
     if count > len(indexes):
         count = len(indexes)
@@ -16,15 +16,14 @@ def display_random_subset(X,y,count=9,order='C',shape=(20,34),labels=[]):
     fig.set_tight_layout(tight=0.1)
     for i,ax in enumerate(axes.ravel()):
         if i <= count:
-            ax.pcolormesh(X[indexes[i],:].reshape((shape),order=order), cmap='magma')
+            ax.pcolormesh(X[indexes[i],:].reshape((mfcc,-1),order=order), cmap='magma')
             ax.set_xticks(())
             ax.set_yticks(())
-            if len(labels) > 0:
-                ax.set_title(labels[y[indexes[i]]])
+            ax.set_title(y[indexes[i]])
         else:
             fig.delaxes(ax)    
             
-def display_class_subset(X,y,category,count=9,order='C',shape=(20,34),labels=None):
+def display_class_subset(X,y,category,count=9,order='C',mfcc=20):
     indexes = (y == category).nonzero()
     indexes = np.random.permutation(indexes)[0]
     if count > len(indexes):
@@ -39,10 +38,9 @@ def display_class_subset(X,y,category,count=9,order='C',shape=(20,34),labels=Non
     fig.set_tight_layout(tight=0.1)
     for i,ax in enumerate(axes.ravel()):
         if i <= count:
-            ax.matshow(X[indexes[i],:].reshape((shape),order=order), cmap='winter')
+            ax.pcolormesh(X[indexes[i],:].reshape((mfcc,-1),order=order), cmap='magma')
             ax.set_xticks(())
             ax.set_yticks(())
-            if len(labels) > 0:
-                ax.set_title(labels[category])
+            ax.set_title(category)
         else:
             fig.delaxes(ax)    
