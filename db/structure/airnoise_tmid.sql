274a0e84315064e481c7f6221014fad9c00d5ea8
alejandECE <alejand@umich.edu>
2020-01-31 14:31:27 -0500

    Update airnoise_tmid.sql

--- db/structure/airnoise_tmid.sql
+++ db/structure/airnoise_tmid.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: localhost    Database: airnoise
 -- ------------------------------------------------------
--- Server version	8.0.17
+-- Server version	8.0.19
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,12 +23,13 @@ DROP TABLE IF EXISTS `tmid`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tmid` (
-  `measurement` int(11) NOT NULL,
-  `microphone` int(11) NOT NULL,
-  `array` int(11) NOT NULL,
+  `measurement` int NOT NULL,
+  `array` int NOT NULL,
+  `microphone` int NOT NULL,
   `location` float NOT NULL DEFAULT '0',
   PRIMARY KEY (`measurement`,`microphone`,`array`),
-  CONSTRAINT `fk_signals` FOREIGN KEY (`measurement`, `microphone`, `array`) REFERENCES `signals` (`measurement`, `microphone`, `array`)
+  KEY `fk_signals` (`measurement`,`array`,`microphone`),
+  CONSTRAINT `fk_signals` FOREIGN KEY (`measurement`, `array`, `microphone`) REFERENCES `signals` (`measurement`, `array`, `microphone`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -41,4 +42,4 @@ CREATE TABLE `tmid` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-08-10 14:28:38
+-- Dump completed on 2020-01-31 14:31:15
