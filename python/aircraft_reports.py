980dea32f3e8bc2ae2fde255d3471634986a2664
alejandECE <alejand@umich.edu>
2020-01-31 14:49:33 -0500

    Changed colormap of the reports

--- python/aircraft_reports.py
+++ python/aircraft_reports.py
@@ -8,15 +8,14 @@ def display_random_subset(X,y,count=9,order='C',mfcc=20):
         count = len(indexes)
     if count <= 0:
         return
-    s = np.sqrt(count)
-    m = int(np.floor(s))
-    n = int(np.ceil(s))
+    m = int(np.ceil(np.sqrt(count)))
+    n = int(np.ceil(count/m))
     fig, axes = plt.subplots(n,m)
     fig.set_size_inches(4*n,4*m)
     fig.set_tight_layout(tight=0.1)
     for i,ax in enumerate(axes.ravel()):
         if i <= count:
-            ax.pcolormesh(X[indexes[i],:].reshape((mfcc,-1),order=order), cmap='magma')
+            ax.pcolormesh(X[indexes[i],:].reshape((mfcc,-1),order=order), cmap='viridis')
             ax.set_xticks(())
             ax.set_yticks(())
             ax.set_title(y[indexes[i]])
@@ -30,15 +29,14 @@ def display_class_subset(X,y,category,count=9,order='C',mfcc=20):
         count = len(indexes)
     if count <= 0:
         return
-    s = np.sqrt(count)
-    m = int(np.floor(s))
-    n = int(np.ceil(s))
+    m = int(np.ceil(np.sqrt(count)))
+    n = int(np.ceil(count/m))
     fig, axes = plt.subplots(n,m)
     fig.set_size_inches(4*n,4*m)
     fig.set_tight_layout(tight=0.1)
     for i,ax in enumerate(axes.ravel()):
         if i <= count:
-            ax.pcolormesh(X[indexes[i],:].reshape((mfcc,-1),order=order), cmap='magma')
+            ax.pcolormesh(X[indexes[i],:].reshape((mfcc,-1),order=order), cmap='viridis')
             ax.set_xticks(())
             ax.set_yticks(())
             ax.set_title(category)
