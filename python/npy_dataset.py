5c31713cccbe5a322e05e4747d46fc5d0ac95096
alejandECE <alejand@umich.edu>
2020-02-09 19:15:49 -0500

    Some reformatting changes

--- python/npy_dataset.py
+++ python/npy_dataset.py
@@ -6,21 +6,23 @@ import librosa
 import datetime
 from scipy.signal import decimate
 
+
 class AircraftFeaturesExtractor(object):
-    '''
+    """
         Extracts specified features from TDMS files (grouped by some class)
-        and exports features to a single file per signal per measurement 
-    '''
+        and exports features to a single file per signal per measurement
+    """
+
     def __init__(self,
                  dbcursor,
                  classes,
                  fs,
-                 feature_type = 'melspectrogram',
-                 segmentation = None,
-                 export = 'npy',
-                 arrays = [1,2,3],
-                 microphones = [1,2,3,4],
-                 logged = True):
+                 feature_type='melspectrogram',
+                 segmentation=None,
+                 export='npy',
+                 arrays=[1, 2, 3],
+                 microphones=[1, 2, 3, 4],
+                 logged=True):
 
         self.fs = fs
         self.dbcursor = dbcursor
@@ -30,8 +32,11 @@ class AircraftFeaturesExtractor(object):
         self.arrays = arrays
         self.microphones = microphones
         self.logged = logged
-                
+
     def build(self):
+        """
+            Goes through every class generating the corresponding npy files
+        """
         # Creates a folder to store dataset
         self._create_root_folder()
         # Creates log file if chosen
@@ -44,7 +49,7 @@ class AircraftFeaturesExtractor(object):
                 # Receives measurements for every aircraft in the class
                 sql = '''SELECT m.id_measurement, m.url FROM 
                             measurements m WHERE m.aircraft IN ({:});'''.format(
-                            str(self.classes[category])[1:-1])
+                    str(self.classes[category])[1:-1])
                 self.dbcursor.execute(sql)
                 results = self.dbcursor.fetchall()
                 # Process all measurements from category
@@ -56,47 +61,57 @@ class AircraftFeaturesExtractor(object):
         self._close_log('Done processing measurements!')
 
     def _process_measurements(self, measurements, category):
+        """
+            For every measurement in the input, reads every signal and extracts the corresponding features, storing the
+            results in a npy file.
+        """
         # Logging
         self._log_message('\tMeasurements included:')
         # Goes through every signal in the measurement
         path = self._create_category_folder(category)
         for measurement, url in measurements:
             # Logging
-            self._log_message('\t\tId: %d, URL: %s' % (measurement,url))
+            self._log_message('\t\tId: %d, URL: %s' % (measurement, url))
             # Opens TDMS file
             tdms = nptdms.TdmsFile("../../datasets/classification/noise/" + url)
             for array in self.arrays:
                 for microphone in self.microphones:
                     # Reads data (actual time series)
                     signal = tdms.channel_data(
-                            group = 'Untitled',
-                            channel = 'cDAQ1Mod' + str(array) +
-                            '/ai' + str(microphone - 1))
+                        group='Untitled',
+                        channel='cDAQ1Mod' + str(array) +
+                                '/ai' + str(microphone - 1))
                     # Extracts features
                     if self.segmentation is None:
-                        features = self._extract_features(signal)             
+                        features = self._extract_features(signal)
                     elif self.segmentation == 'tmid':
                         window = 5
-                        tmid = self._get_tmid(measurement,microphone,array)
-                        start = int((tmid - window)*self.fs) 
-                        end = int((tmid + window)*self.fs) 
-                        features = self._extract_features(signal[start:end+1])
+                        tmid = self._get_tmid(measurement, array, microphone)
+                        start = int((tmid - window) * self.fs)
+                        end = int((tmid + window) * self.fs)
+                        features = self._extract_features(signal[start:end + 1])
                     # Stores features
-                    name = 'm{}a{}s{}.npy'.format(measurement,array,microphone)
-                    np.save(os.path.join(path,name),features)
+                    name = 'm{}a{}s{}.npy'.format(measurement, array, microphone)
+                    np.save(os.path.join(path, name), features)
 
     def _extract_features(self, data):
-        if (self.feature_type == 'melspectrogram'):
+        """
+            Extracts the actual mel-spectrogram for the given data
+        """
+        if self.feature_type == 'melspectrogram':
             return self._extract_melspectrogram(data)
-        
-    def _get_tmid(self,measurement,microphone,array):
+
+    def _get_tmid(self, measurement, array, microphone):
+        """
+            Gets the tmid point from the dataset for the given measurement, array and microphone
+        """
         sql = ('''SELECT t.location FROM tmid t WHERE
                t.measurement = {:} AND
                t.array = {:} AND
                t.microphone = {:}''').format(measurement, array, microphone)
         self.dbcursor.execute(sql)
         result = self.dbcursor.fetchall()
-                        
+
         if len(result) > 0:
             return result[0][0]
         else:
@@ -108,52 +123,54 @@ class AircraftFeaturesExtractor(object):
         # Mel spectrogram configuration
         factor = 6
         mfcc = 128
-        n_fft=1024
-        hop_length=512        
+        n_fft = 1024
+        hop_length = 512
         # Decimate signal by factor (reducing sampling rate)
-        data = decimate(data,factor)
+        data = decimate(data, factor)
         # Computes mel spectrogram
         spectrogram = librosa.feature.melspectrogram(np.asfortranarray(data),
-                                                     sr=int(self.fs/factor),
+                                                     sr=int(self.fs / factor),
                                                      n_fft=n_fft,
                                                      hop_length=hop_length,
                                                      n_mels=mfcc)
         return spectrogram
-    
+
     def _create_root_folder(self):
         self.root_path = 'exports/' + datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
         os.mkdir(self.root_path)
-    
-    def _create_category_folder(self,category):
+
+    def _create_category_folder(self, category):
         path = os.path.join(self.root_path, str(category))
         os.mkdir(path)
         return path
-    
+
     def _create_log(self):
         if self.logged:
-            self.log_file = open(os.path.join(self.root_path, 'log.txt') ,'w')
-        
-    def _log_message(self,msg):
+            self.log_file = open(os.path.join(self.root_path, 'log.txt'), 'w')
+
+    def _log_message(self, msg):
         if self.logged:
             print(msg)
             self.log_file.write(msg + '\n')
-            
-    def _close_log(self,msg=None):
+
+    def _close_log(self, msg=None):
         if self.logged:
             if msg is not None:
                 self._log_message(msg)
             self.log_file.close()
-                    
+
+
 class AircraftDatasetFoldIterator():
-    '''
+    """
         Generates k-fold stratified folds for the aircraft dataset.
-        
+
         It ensures the number of samples per class on each fold is as even as
         possible. During validation for traning it includes all signals from a
         measurement but for test only one random signal from each measurement
         is included.
-    '''
-    def __init__(self,X,y,measurements,folds=5):
+    """
+
+    def __init__(self, X, y, measurements, folds=5):
         self.current = 0
         self.X = X
         self.y = y
@@ -164,7 +181,9 @@ class AircraftDatasetFoldIterator():
             self._splits.append([])
 
     def build(self):
-        '''Build k stratified folds using measurements and reponses provided'''
+        """
+            Build k stratified folds using measurements and reponses provided
+        """
         classes = np.unique(self.y)
         for category in classes:
             # Determines measurements for that class
@@ -175,11 +194,11 @@ class AircraftDatasetFoldIterator():
             if overall_count < self.folds:
                 raise Exception('''Class {} only has {} measurements, minimum
                                 required is {} to create {} folds'''.format(
-                                category,overall_count,self.folds,self.folds))
+                    category, overall_count, self.folds, self.folds))
             curr = 0
             fold_count = int(np.floor(overall_count / self.folds))
             for k in range(self.folds):
-                for index in indexes[curr:curr+fold_count]:
+                for index in indexes[curr:curr + fold_count]:
                     self._splits[k].append(index)
                 curr += fold_count
             for index in indexes[curr:]:
@@ -191,7 +210,7 @@ class AircraftDatasetFoldIterator():
     def __next__(self):
         # Builds test observations
         test_obs = []
-        if (self.current >= len(self._splits)):
+        if self.current >= len(self._splits):
             self.current = 0
             raise StopIteration
         indexes = self._splits[self.current]
@@ -201,13 +220,13 @@ class AircraftDatasetFoldIterator():
             test_obs.append(observations[i])
         # Builds train observations
         train_obs = []
-        for k,indexes in enumerate(self._splits):
+        for k, indexes in enumerate(self._splits):
             if k == self.current:
                 continue
             for index in indexes:
                 observations = (self.measurements == index).nonzero()[0]
-                train_obs = train_obs + [i for i in observations]                
-        # Updates iteration
+                train_obs = train_obs + [i for i in observations]
+                # Updates iteration
         self.current += 1
         # Return indexes
-        return (train_obs,test_obs)
+        return train_obs, test_obs
--- python/tfrecord_dataset.py
+++ python/tfrecord_dataset.py
@@ -5,53 +5,64 @@ import random
 import numpy as np
 import tensorflow as tf
 
+# Dictionary containing features description for parsing purposes
+feature_description = {
+    'spec': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True, default_value=[0.0]),
+    'label': tf.io.FixedLenFeature([], tf.string, default_value=''),
+    'measurement': tf.io.FixedLenFeature([], tf.string, default_value=''),
+    'array': tf.io.FixedLenFeature([], tf.string, default_value=''),
+    'sensor': tf.io.FixedLenFeature([], tf.string, default_value='')
+}
+
+
 class AircraftRecordBuilder(object):
-    '''
+    """
         Creates two records files (training/test)
         It ensures the percentage of test samples of each class is the same.
         For the traning set it includes all signals from a measurement but for
         the test set only one random signal from each measurement is
         included.
-    '''
+    """
+
     def __init__(self, path):
         # Stores path
         self.path = path
         # Reads all npy files found inside path
         self.datafiles = []
-        for root,_,files in os.walk(path):
+        for root, _, files in os.walk(path):
             for file in files:
                 if '.npy' in file:
-                    groups = re.match(r'^m(\d+)a(\d+)s(\d+)',file).groups()
-                    url = os.path.join(root,file)
+                    groups = re.match(r'^m(\d+)a(\d+)s(\d+)', file).groups()
+                    url = os.path.join(root, file)
                     label = os.path.split(root)[1]
                     measurement = groups[0]
                     array = groups[1]
-                    sensor = groups[2]                    
+                    sensor = groups[2]
                     # appending (url, class name, measurement id, array, sensor)
-                    self.datafiles.append((url,label,measurement,array,sensor))
+                    self.datafiles.append((url, label, measurement, array, sensor))
         random.shuffle(self.datafiles)
-    
-    '''
-        Generates a separate tfrecord file containing the serialized observations
-        for each stratified set (training & test) generated from the signals
-        found in the input path.
-    '''
+
     def build(self, test_pct=0.2):
+        """
+            Generates a separate tfrecord file containing the serialized observations
+            for each stratified set (training & test) generated from the signals
+            found in the input path.
+        """
         # generate sets of files
         train_set, test_set = self.generate_sets(test_pct)
         # generate a tfrecord for each set
-        self.generate_tfrecord(train_set,'train.tfrecord')
-        self.generate_tfrecord(test_set,'test.tfrecord')
-    
-    '''
-        Creates stratified training and test sets of the signals found in the
-        input path 
-    '''
+        self.generate_tfrecord(train_set, 'train.tfrecord')
+        self.generate_tfrecord(test_set, 'test.tfrecord')
+
     def generate_sets(self, test_pct):
+        """
+            Creates stratified training and test sets of the signals found in the
+            input path
+        """
         # Shuffles dataset
         measurements = []
         labels = []
-        for _,label,measurement,_,_ in self.datafiles:
+        for _, label, measurement, _, _ in self.datafiles:
             measurements.append(measurement)
             labels.append(label)
         measurements = np.array(measurements)
@@ -59,8 +70,8 @@ class AircraftRecordBuilder(object):
         classes = np.unique(labels)
         # Splits dataset
         m = len(self.datafiles)
-        train_obs = np.zeros(m,dtype=bool)
-        test_obs = np.zeros(m,dtype=bool)
+        train_obs = np.zeros(m, dtype=bool)
+        test_obs = np.zeros(m, dtype=bool)
         for category in classes:
             indexes = np.unique(measurements[labels == category])
             indexes = np.random.permutation(indexes)
@@ -75,38 +86,39 @@ class AircraftRecordBuilder(object):
                 mask = np.random.permutation(new_obs.nonzero()[0])
                 new_obs[mask[:-1]] = False
                 test_obs = test_obs | new_obs
-                
-        train_set = [self.datafiles[i] for i in range(len(train_obs)) if train_obs[i]] 
+
+        train_set = [self.datafiles[i] for i in range(len(train_obs)) if train_obs[i]]
         test_set = [self.datafiles[i] for i in range(len(test_obs)) if test_obs[i]]
-           
+
         return train_set, test_set
-    
-    '''
-        Generates a tfrecord file containing the serialized observations 
-    '''
+
     def generate_tfrecord(self, observations, filename):
-        filepath = os.path.join(self.path,filename)
+        """
+            Generates a tfrecord file containing the serialized observations
+        """
+        filepath = os.path.join(self.path, filename)
         with tf.io.TFRecordWriter(filepath) as writer:
             for obs in observations:
-                example = self._serialize(obs)
+                example = _serialize(obs)
                 writer.write(example)
-    
-    '''
+
+
+def _serialize(observation):
+    """
         Serializes a single observation
-    ''' 
-    def _serialize(self, observation):
-        # Read npy file
-        url,label,measurement,array,sensor = observation
-        spectrogram = np.load(url)
-        # Create a dictionary mapping the feature name to the tf.Example compatible data type
-        feature = {
-            'spec': tf.train.Feature(float_list=tf.train.FloatList(value=list(spectrogram.ravel()))),
-            'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[label.encode()])),
-            'measurement': tf.train.Feature(bytes_list=tf.train.BytesList(value=[measurement.encode()])),
-            'array': tf.train.Feature(bytes_list=tf.train.BytesList(value=[array.encode()])),
-            'sensor': tf.train.Feature(bytes_list=tf.train.BytesList(value=[sensor.encode()]))            
-        }        
-        # Create a Features message using tf.train.Example
-        example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
-        
-        return example_proto.SerializeToString()
+    """
+    # Read npy file
+    url, label, measurement, array, sensor = observation
+    spectrogram = np.load(url)
+    # Create a dictionary mapping the feature name to the tf.Example compatible data type
+    feature = {
+        'spec': tf.train.Feature(float_list=tf.train.FloatList(value=list(spectrogram.ravel()))),
+        'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[label.encode()])),
+        'measurement': tf.train.Feature(bytes_list=tf.train.BytesList(value=[measurement.encode()])),
+        'array': tf.train.Feature(bytes_list=tf.train.BytesList(value=[array.encode()])),
+        'sensor': tf.train.Feature(bytes_list=tf.train.BytesList(value=[sensor.encode()]))
+    }
+    # Create a Features message using tf.train.Example
+    example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
+
+    return example_proto.SerializeToString()
