327d78e6db54f8612c13518077deea28f9a4b6e3
alejandECE <alejand@umich.edu>
2020-02-29 19:07:02 -0500

    Adds RCNN model for multiclass problem

--- python/aircraft_models.py
+++ python/aircraft_models.py
@@ -63,7 +63,7 @@ class AirMultinomialTemporalCNN:
       Simple CNN model with two 1D conv layers followed by global max pooling
       and a fully connected layer.
 
-      Desgined to perform multinomial classification on a simple dataset
+      Designed to perform multinomial classification on a simple dataset
       containing four classes of aircraft take-off signals.
   """
 
@@ -111,7 +111,6 @@ class AirMultinomialTemporalCNN:
     outputs = dense1(x)
 
     self.model = tf.keras.Model(inputs, outputs)
-
     return self.model
 
 
@@ -140,19 +139,6 @@ class AirBinaryRNN:
 
       Designed to perform binary classification on a simple dataset containing
       Airbus/Boeing aircraft take-off signals.
-  """
-  def __init__(self, use_regularizer=True):
-    self.use_regularizer = use_regularizer
-    self.model = None
-
-  def build_model(self, input_shape):
-    pass
-class AirMultinomialRNN:
-  """
-      Simple recurrent neural network model using a sequence of spectrogram windows
-
-      Designed to perform multinomial classification on a simple dataset
-      containing four classes of aircraft take-off signals.
   """
 
   def __init__(self, use_regularizer=True):
@@ -186,12 +172,20 @@ class AirMultinomialRNN:
       tf.keras.layers.Flatten(),
       name='Flatten'
     )
+    # Dropout layer
+    dropout1 = tf.keras.layers.Dropout(0.3)
     # Recurrent layer to capture temporal relationships
     lstm1 = tf.keras.layers.LSTM(32, return_state=False, return_sequences=False)
-    # Global max pooling to keep the best "output"
-    global1 = tf.keras.layers.GlobalMaxPool1D(name='GlobalPool1D')
+    # Dropout layer
+    dropout2 = tf.keras.layers.Dropout(0.1)
     # Dense to make the final classification
-    dense1 = tf.keras.layers.Dense(1, activation=tf.nn.sigmoid, name='Dense')
+    if self.use_regularizer:
+      dense1 = tf.keras.layers.Dense(1, activation=tf.nn.sigmoid,
+                                     kernel_regularizer=
+                                     tf.keras.regularizers.l2(0.3),
+                                     name='Dense')
+    else:
+      dense1 = tf.keras.layers.Dense(1, activation=tf.nn.sigmoid, name='Dense')
 
     # Create connections and returns model
     inputs = tf.keras.layers.Input(input_shape)
@@ -200,7 +194,86 @@ class AirMultinomialRNN:
     x = conv2(x)
     x = pooling2(x)
     x = flatten1(x)
+    if self.use_regularizer:
+      x = dropout1(x)
     x = lstm1(x)
+    if self.use_regularizer:
+      x = dropout2(x)
+    outputs = dense1(x)
+
+    self.model = tf.keras.Model(inputs, outputs)
+    return self.model
+
+
+class AirMultinomialRNN:
+  """
+      Simple recurrent neural network model using a sequence of spectrogram windows
+
+      Designed to perform multinomial classification on a simple dataset
+      containing four classes of aircraft take-off signals.
+  """
+
+  def __init__(self, categories, use_regularizer=True):
+    self.use_regularizer = use_regularizer
+    self.categories = categories
+    self.model = None
+
+  def build_model(self, input_shape):
+    # First convolutional layer to find spatial features in a segment of the spectrogram. Timed distributed to get
+    # applied to every segment of the inputted spectrogram.
+    conv1 = tf.keras.layers.TimeDistributed(
+      tf.keras.layers.Conv2D(32, 5, padding='valid', activation=tf.nn.relu, data_format='channels_last'),
+      name='Conv1'
+    )
+    # Pooling
+    pooling1 = tf.keras.layers.TimeDistributed(
+      tf.keras.layers.MaxPool2D(3),
+      name='Pool1'
+    )
+    # Second convolutional layer. Timed distributed to get applied to every segment of the inputted spectrogram.
+    conv2 = tf.keras.layers.TimeDistributed(
+      tf.keras.layers.Conv2D(32, 5, padding='valid', activation=tf.nn.relu, data_format='channels_last'),
+      name='Conv2'
+    )
+    # Pooling
+    pooling2 = tf.keras.layers.TimeDistributed(
+      tf.keras.layers.MaxPool2D(3),
+      name='Pool2'
+    )
+    # Flatten results from convolutions/pooling to be fed to the lstm layers
+    flatten1 = tf.keras.layers.TimeDistributed(
+      tf.keras.layers.Flatten(),
+      name='Flatten'
+    )
+    # Dropout layer
+    dropout1 = tf.keras.layers.Dropout(0.4)
+    # Recurrent layer to capture temporal relationships
+    lstm1 = tf.keras.layers.Bidirectional(
+      tf.keras.layers.LSTM(32, return_state=False, return_sequences=True),
+      name='Bidirectional'
+    )
+    # Global max pool
+    global1 = tf.keras.layers.GlobalMaxPool1D()
+    # Dense to make the final classification
+    if self.use_regularizer:
+      dense1 = tf.keras.layers.Dense(self.categories, activation=tf.nn.softmax,
+                                     kernel_regularizer=
+                                     tf.keras.regularizers.l2(0.2),
+                                     name='Dense')
+    else:
+      dense1 = tf.keras.layers.Dense(self.categories, activation=tf.nn.softmax, name='Dense')
+
+    # Create connections and returns model
+    inputs = tf.keras.layers.Input(input_shape)
+    x = conv1(inputs)
+    x = pooling1(x)
+    x = conv2(x)
+    x = pooling2(x)
+    x = flatten1(x)
+    if self.use_regularizer:
+      x = dropout1(x)
+    x = lstm1(x)
+    x = global1(x)
     outputs = dense1(x)
 
     self.model = tf.keras.Model(inputs, outputs)
